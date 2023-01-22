f0ee82d1283553d6b2cd2d119a11ee815b831217
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-01-22 04:13:34 -0800

    new change

--- src/components/Best seller/BestSeller.jsx
+++ src/components/Best seller/BestSeller.jsx
@@ -14,6 +14,7 @@ import { Pagination } from "swiper";
 export default function BestSeller() {
   return (
     <>
+    <div className="bestH"><h1>Best Seller</h1></div>
       <Swiper
         slidesPerView={1}
         spaceBetween={10}
@@ -37,12 +38,18 @@ export default function BestSeller() {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide className="swipe" ><img className=""  src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1607103555-51wmv-2LokL.jpg?crop=1xw:0.993xh;center,top&resize=480:*" /></SwiperSlide>
-        <SwiperSlide className="swipe"><img className="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUmNCPrkvcUqV_TNJ3OUfgcW8_0IBgkWkXU2BH_C1_7cWIci3Q8vJfT5sP7l0Fo2Vq74&usqp=CAU" /></SwiperSlide>
-        <SwiperSlide className="swipe"><img className="" src='https://images1.penguinrandomhouse.com/cover/9780735211247' /></SwiperSlide>
-        <SwiperSlide className="swipe"><img className="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSflVQHazUTLkqbp7TQNd2ACNaif-fdgxjwrw&usqp=CAU" /></SwiperSlide>
-        <SwiperSlide className="swipe"><img className="" src="https://images-na.ssl-images-amazon.com/images/I/51E96WWXW1L._AC_SX368_.jpg" /></SwiperSlide>
-        <SwiperSlide className="swipe"><img className="" src="https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781476753188/ugly-love-9781476753188_lg.jpg" /></SwiperSlide>
+        <SwiperSlide className="swipe" > <a href="#"><img className="swipImg"  src=" https://images4.penguinrandomhouse.com/cover/9780385474542" /></a></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://images1.penguinrandomhouse.com/cover/9781984821867" /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src='https://images1.penguinrandomhouse.com/cover/9780735211247' /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSflVQHazUTLkqbp7TQNd2ACNaif-fdgxjwrw&usqp=CAU" /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://images3.penguinrandomhouse.com/cover/9780593337820" /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781476753188/ugly-love-9781476753188_lg.jpg" /></SwiperSlide>
+        <SwiperSlide className="swipe" ><img className="swipImg"  src="https://images4.penguinrandomhouse.com/cover/9780593498958" /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://images4.penguinrandomhouse.com/cover/9780593539941" /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src='https://images4.penguinrandomhouse.com/cover/9780593201268' /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://images1.penguinrandomhouse.com/cover/9780593156599" /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://images3.penguinrandomhouse.com/cover/9780399592706" /></SwiperSlide>
+        <SwiperSlide className="swipe"><img className="swipImg" src="https://images3.penguinrandomhouse.com/cover/9780812976717" /></SwiperSlide>
        
       </Swiper>
     </>
